b66d7408895461a15a9f94ef88a5cb297fe6ddf4
赵劲焰 <949474952@qq.com>
2019-03-25 22:00:52 +0800

    修改了科目的编码

--- src/main/script/book.sql
+++ src/main/script/book.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS `abstract`;
 CREATE TABLE abstract
 (
   id          INT PRIMARY KEY AUTO_INCREMENT COMMENT '主键',
-  content     VARCHAR(255) NOT NULL DEFAULT '' COMMENT '内容',
-  create_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  update_time TIMESTAMP    NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更行时间'
+  content     VARCHAR(255)       DEFAULT '' NOT NULL COMMENT '内容',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  update_time TIMESTAMP NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更行时间'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   AUTO_INCREMENT = 1
@@ -15,17 +15,17 @@ CREATE TABLE abstract
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE book
 (
-  id                   INT PRIMARY KEY AUTO_INCREMENT
+  id                   INT                                                             NOT NULL PRIMARY KEY AUTO_INCREMENT
     COMMENT '',
-  name                 VARCHAR(50) NOT NULL
+  name                 VARCHAR(50)                                                     NOT NULL
     COMMENT '',
-  account_date         CHAR(8)     NOT NULL DEFAULT '00000000',
-  closing_account_date CHAR(8)     NOT NULL DEFAULT '00000000',
-  create_time          TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+  account_date         CHAR(8)   DEFAULT '00000000'                                    NOT NULL,
+  closing_account_date CHAR(8)   DEFAULT '00000000'                                    NOT NULL,
+  create_time          TIMESTAMP DEFAULT CURRENT_TIMESTAMP                             NOT NULL
     COMMENT '创建时间',
-  update_time          TIMESTAMP   NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  update_time          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL
     COMMENT '更行时间',
-  abstract_id          INT         NOT NULL DEFAULT 0 COMMENT '摘要',
+  abstract_id          INT       DEFAULT 0                                             NOT NULL COMMENT '摘要',
   UNIQUE KEY (name)
 )
   ENGINE = InnoDB
@@ -33,22 +33,21 @@ CREATE TABLE book
   AUTO_INCREMENT = 1
   COMMENT '账本';
 
+DROP TABLE IF EXISTS `subject`;
 CREATE TABLE subject
 (
-  id          CHAR(8) PRIMARY KEY
-    COMMENT '主键',
-  book_id     INT          NOT NULL DEFAULT 0,
-  path        VARCHAR(255) NOT NULL UNIQUE
-    COMMENT '路径',
-  name        VARCHAR(30)  NOT NULL,
-  pid         INT          NOT NULL DEFAULT 0,
-  create_time TIMESTAMP    NOT NULL DEFAULT current_timestamp
-    COMMENT '创建时间',
-  update_time TIMESTAMP    NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    COMMENT '更行时间'
+  `id`          CHAR(8) PRIMARY KEY COMMENT '主键',
+  `book_id`     INT          NOT NULL DEFAULT 0 COMMENT '账本ID',
+  `path`        VARCHAR(255) NOT NULL UNIQUE COMMENT '路径',
+  `name`        VARCHAR(30)  NOT NULL,
+  `pid`         INT          NOT NULL DEFAULT 0,
+  `create_time` TIMESTAMP    NOT NULL DEFAULT current_timestamp COMMENT '创建时间',
+  `update_time` TIMESTAMP    NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更行时间',
+  `abstract_id` INT          NOT NULL DEFAULT 0 COMMENT '摘要',
+  UNIQUE KEY (name)
 )
   ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
+  DEFAULT CHARSET = utf8
   AUTO_INCREMENT = 1
   COMMENT '科目';
 
