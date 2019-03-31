c27d56bdbed6bf02722318fc36274ef49debb9f7
赵劲焰 <949474952@qq.com>
2019-03-31 22:03:54 +0800

    + subjectDao

---
+++ src/main/java/com/book/dao/SubjectDao.java
@@ -0,0 +1,11 @@
+package com.book.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.book.entity.Subject;
+
+/**
+ * @author Zhao Jinyan
+ * @date 2019/3/31 21:51
+ */
+public interface SubjectDao extends BaseMapper<Subject> {
+}
